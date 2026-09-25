09135315e509a30ff4659af0b08c3e0edd77d614
anshsaini8445 <anshsaini8445@gmail.com>
2026-09-25 11:00:08 +0530

    Create MainActivity for media player app

---
+++ app/src/main/java/com/app/mediaplayer/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.app.mediaplayer
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        // Agent will add UI here automatically
+    }
+}
